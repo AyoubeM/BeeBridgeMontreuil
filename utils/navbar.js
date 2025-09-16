a3d0d33ec74841cd8e84bdbae84ee441f97ceb5c
AyoubeM <berudevil@gmail.com>
2025-09-16 14:09:40 +0200

    ajout de la navbar

---
+++ utils/navbar.js
@@ -0,0 +1,6 @@
+fetch("/utils/navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(err => console.error("Erreur chargement navbar:", err));
