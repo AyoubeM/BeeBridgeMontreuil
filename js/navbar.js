13d0b7bd6fdd24d92c0cb7ac6e3a70e9b51a3918
AyoubeM <berudevil@gmail.com>
2025-09-16 17:22:41 +0200

    correction bug navbar et trad

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,17 @@
-// Injecter la navbar
 fetch("partial/navbar.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
+
+    // Maintenant la navbar est chargée → on peut trouver le select
+    const langSwitcher = document.getElementById("langSwitcher");
+    if (langSwitcher) {
+      langSwitcher.addEventListener("change", e => {
+        loadLang(e.target.value);
+      });
+
+      // Appliquer directement la langue enregistrée
+      loadLang(localStorage.getItem("lang") || "fr");
+    }
   })
   .catch(err => console.error("Erreur navbar:", err));
