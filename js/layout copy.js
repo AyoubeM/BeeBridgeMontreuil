964bee12f73daa4a040683c776096b0f07631882
AyoubeM <berudevil@gmail.com>
2025-11-30 18:03:24 +0100

    kkkkkkkkk

---
+++ js/layout copy.js
@@ -0,0 +1,26 @@
+fetch("partial/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch(err => console.error("Erreur Footer:", err));
+
+fetch("partial/navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    // Changer de langue
+    const langSwitcher = document.getElementById("langSwitcher");
+    if (langSwitcher) {
+      langSwitcher.addEventListener("change", e => {
+        loadLang(e.target.value);
+      });
+
+      // Appliquer directement la langue enregistrée
+      loadLang(localStorage.getItem("lang") || "fr");
+    }
+  })
+  .catch(err => console.error("Erreur navbar:", err));
+  
+  
