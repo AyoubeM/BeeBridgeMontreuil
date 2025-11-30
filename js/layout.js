f636967844e273398bb7e3cc8fc4abaf6f8ecaed
cacharge101 <maxbeloucif@gmail.com>
2025-11-30 17:21:13 +0100

    fsfsf

--- js/layout.js
+++ js/layout.js
@@ -22,5 +22,20 @@ fetch("partial/navbar.html")
     }
   })
   .catch(err => console.error("Erreur navbar:", err));
-  
-  
+
+function initFAQ() {
+  const faqQuestions = document.querySelectorAll(".faq-question");
+
+  faqQuestions.forEach(question => {
+    question.addEventListener("click", () => {
+      // Toggle flèche
+      question.classList.toggle("active");
+
+      // Réponse correspondante
+      const answer = question.nextElementSibling;
+      answer.classList.toggle("hidden");
+    });
+  });
+}
+// pour eviter les crash
+document.addEventListener("DOMContentLoaded", initFAQ);
