ef4581a5d13f41ed3343f906146a38d25344ccb0
AyoubeM <berudevil@gmail.com>
2025-12-05 18:36:17 +0100

    ddddddddddd

--- js/lang.js
+++ js/lang.js
@@ -1,82 +1,3 @@
-// let currentLang;
-
-// // Détection langue enregistrée ou navigateur
-// if (localStorage.getItem("lang")) {
-//   currentLang = localStorage.getItem("lang");
-// } else {
-//   currentLang = navigator.language.startsWith("en") ? "en" : "fr";
-//   localStorage.setItem("lang", currentLang);
-// }
-
-// // ----------- CHARGER LA LANGUE -----------
-// function loadLang(lang) {
-
-//   // Toujours utiliser un chemin ABSOLU, sinon 404
-//   fetch("/lang/" + lang + ".json")
-//     .then(res => res.json())
-//     .then(data => {
-//       document.querySelectorAll("[data-translate]").forEach(el => {
-//         let key = el.getAttribute("data-translate");
-//         if (data[key]) {
-//           el.textContent = data[key];
-//         }
-//       });
-//     })
-//     .catch(err => console.error("Erreur langue:", err));
-
-//   // Sauvegarder la langue
-//   localStorage.setItem("lang", lang);
-
-//   // Mettre à jour le drapeau
-//   updateFlag(lang);
-// }
-
-
-// // ----------- METTRE À JOUR LE DRAPEAU -----------
-// function updateFlag(lang) {
-
-//   let img = document.querySelector("#langSwitcher img");
-
-//   // Si navbar n'est pas encore chargée → on retry
-//   if (!img) {
-//     setTimeout(() => updateFlag(lang), 100);
-//     return;
-//   }
-
-//   if (lang === "fr") {
-//     img.src = "/media/fr.png";
-//     img.alt = "Français";
-//   } else {
-//     img.src = "/media/uk.png";
-//     img.alt = "English";
-//   }
-// }
-
-
-// // ----------- INITIALISER SWITCHER APRÈS CHARGEMENT NAVBAR -----------
-// function initLangSwitcher() {
-
-//   const btn = document.getElementById("langSwitcher");
-
-//   if (!btn) {
-//     // Navbar encore pas chargée → retry
-//     setTimeout(initLangSwitcher, 100);
-//     return;
-//   }
-
-//   btn.addEventListener("click", () => {
-//     currentLang = currentLang === "fr" ? "en" : "fr";
-//     loadLang(currentLang);
-//   });
-// }
-
-
-// // ----------- LANCER LORSQUE TOUT EST CHARGÉ -----------
-// document.addEventListener("DOMContentLoaded", () => {
-//   initLangSwitcher();
-//   loadLang(currentLang);
-// });
-
 let currentLang;
 
 // Détection langue enregistrée ou navigateur
@@ -89,24 +10,17 @@ if (localStorage.getItem("lang")) {
 
 // ----------- CHARGER LA LANGUE -----------
 function loadLang(lang) {
-
-  // Toujours utiliser un chemin ABSOLU, sinon 404
-  fetch("lang/" + lang + ".json")
+  fetch("/BeeBridgeMontreuil/lang/" + lang + ".json")
     .then(res => res.json())
     .then(data => {
       document.querySelectorAll("[data-translate]").forEach(el => {
         let key = el.getAttribute("data-translate");
-        if (data[key]) {
-          el.textContent = data[key];
-        }
+        if (data[key]) el.textContent = data[key];
       });
     })
     .catch(err => console.error("Erreur langue:", err));
 
-  // Sauvegarder la langue
   localStorage.setItem("lang", lang);
-
-  // Mettre à jour le drapeau
   updateFlag(lang);
 }
 
